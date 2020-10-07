a03cc23fd9e1303d44a4a42e3a53529cbe4039ee
jesssubin <jess.s.moon@gmail.com>
2020-10-07 22:22:04 +0000

    Add function to prevent cross-site scripting

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,33 +5,17 @@
  */
 
 $(document).ready(function () {
- 
+  $('#tweet-form').submit(submitHandler);
 
-  const data = [
-    {
-      "user": {
-        "name": "Einstein",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@Lightbulb"
-      },
-      "content": {
-        "text": "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe."
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+  loadTweets(); 
+});
+
+//prevents cross-site scripting 
+const escape = function (str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}; 
 
   const createTweetElement = function (tweetData) {
     const $tweet = $(`<article class="tweet-container">
@@ -43,11 +27,11 @@ $(document).ready(function () {
             <p class="userid">${tweetData.user.handle}</p>
           </div>
           <div class="tweet-txt">
-            <p class="tweet-content">${tweetData.content.text}</p>
+            <p class="tweet-content">${escape(tweetData.content.text)}</p>
             <p class="bottom-border"></p>
           </div>
           <footer class="footer" >
-            <p class="date-update"> 10 days ago</p>
+            <p class="date-update">${moment(tweetData.created_at).fromNow()}</p>
             <div class="icons">
             <i class="fa fa-flag fa-xs"></i>
             <i class="fa fa-retweet fa-xs"></i>
@@ -56,20 +40,46 @@ $(document).ready(function () {
           </footer>
         </article> `);
     return $tweet;
-  }
+  };
 
   const renderTweets = function (tweets) {
     let $element; 
+    const $container = $('#tweets-section'); 
+    $container.html(""); 
     // loops through tweets
     for (const tweet of tweets) {
       // calls createTweetElement for each tweet
       $element = createTweetElement(tweet); 
       // takes return value and appends it to the tweets container
-      $('#tweets-section').append($element);
+      $container.prepend($element);
     }
+  };
+
+    const submitHandler = function (event) {
+      event.preventDefault();
+      const $form = $(this); 
+      const text = $("#tweet-text").val(); 
+      console.log("text: ", text); 
+      if(!text || text.length > 140 || !text.length) {
+        alert("Invalid Tweet"); 
+        return; 
+      }
+      console.log('Button clicked, performing ajax call...');
+      console.log($(this).serialize()); 
+      $.ajax('/tweets', { method: 'POST', data: $form.serialize()})
+        .then(function (data) {
+          $("#tweet-text").val(""); 
+          loadTweets(); 
+        });
+    };
   
-  }
+  const loadTweets = function () {
+      $.ajax('/tweets', { method: 'GET' })
+        .then(function (data) {
+          renderTweets(data); 
+        });
+    };
+  
+    
+    
 
-  renderTweets(data); 
-
-});
